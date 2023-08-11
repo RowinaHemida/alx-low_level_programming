053c26b05214cd3115f4e1a35495f69f8fed74f9
Rowina Hemida <eng.rowinatamer@gmail.com>
2023-08-11 08:46:31 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,15 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Rowina Hemida
+ */
+
 #include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char letter;

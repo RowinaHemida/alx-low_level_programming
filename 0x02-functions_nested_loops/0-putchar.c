523512e7c151c4e97ebda6170bba38997a51ce7c
Rowina Hemida <eng.rowinatamer@gmail.com>
2023-08-15 13:35:08 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,8 @@
-#include "main.h"
-
+#include <unistd.h>
 /**
-*main - function prints text as output
-*
-*Return:return 0
-*/
-int main(void)
+ * main 
+ */
+int _putchar(char c)
 {
-	char school[10] = "_putchar";
-
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(school[i]);
-	}
-	_putchar(10);
-	return (0);
+	return (write(1, &c, 1));
 }

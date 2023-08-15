a28599dfd0421a57548fecb7270bc199cd3f880c
Rowina Hemida <eng.rowinatamer@gmail.com>
2023-08-15 12:36:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main 
+ */
+int main(void)
+{
+	char c[] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

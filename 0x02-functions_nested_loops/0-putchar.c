b1b147bcf60fe63a227128aaa616b2cf3088f872
Rowina Hemida <eng.rowinatamer@gmail.com>
2023-08-15 14:47:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main
- * return: Alwyas 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -17,5 +16,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
